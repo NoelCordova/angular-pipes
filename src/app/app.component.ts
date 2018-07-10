f77b3f1a5dc935231814ca8ed110c8b079fd5e23
Noel Cordova <ednoesco_7@hotmail.com>
2018-07-09 22:26:31 -0500

    build-in pipes and capitalized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { resolve } from 'path';
+// import { reject } from '../../node_modules/@types/q';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  nombre:string = "Noel";
+
+  arreglo:any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  PI:number = Math.PI;
+
+  a:number = 0.234;
+
+  costo:number = 12345.67;
+
+  directorio:{} = {
+    nombre: {
+      nombres: "Diana Karina",
+      appat: "Medina",
+      apmat: "Collazo"
+    },
+    edad: 23,
+    direccion: {
+      calle: "Alguna loma",
+      numero: 123
+    }
+  };
+
+  // promise = new Promise((resolve, reject) => {
+  //   setTimeout(() => resolve('Llegó la información!'), 3500);
+  // });
+
+  frase:string = "ora prro, KYC ALV mushasho";
+
 }
---
+++ src/app/pipes/capitalizado.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({name: 'capitalizado'})
+export class CapitalizadoPipe implements PipeTransform {
+  transform(value: string): string {
+    
+    value = value.toLowerCase();
+
+    let palabras = value.split(" ");
+
+    for (let i in palabras) {
+      
+      palabras[i] = palabras[i][0].toUpperCase() + palabras[i].substr(1);
+
+    }
+
+    return palabras.join(" ");
+  }
+}
